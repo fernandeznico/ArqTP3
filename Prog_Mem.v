9dab951a6d201af8235778fd0a50f74500379914
fernandeznico <nicofernandez@alumnos.unc.edu.ar>
2017-11-09 01:54:57 -0300

    add Data_Mem.v
    Next: Test Prog_Mem.v and Data_Mem.v

--- Prog_Mem.v
+++ Prog_Mem.v
@@ -1,7 +1,7 @@
 module Prog_Mem
 	#(
 		parameter addr_bus = 11 ,
-					 data_size = 16
+				  data_size = 16
 	)
 	(
 		input wire Reset ,
@@ -10,23 +10,23 @@ module Prog_Mem
 	);
 
 localparam [4:0]
-	HLT	= 5'b00000 ,
-	STO	= 5'b00001 ,
-	LD_	= 5'b00010 ,
-	LDI	= 5'b00011 ,
-	ADD	= 5'b00100 ,
+	HLT		= 5'b00000 ,
+	STO		= 5'b00001 ,
+	LD_		= 5'b00010 ,
+	LDI		= 5'b00011 ,
+	ADD		= 5'b00100 ,
 	ADDI	= 5'b00101 ,
-	SUB	= 5'b00110 ,
+	SUB		= 5'b00110 ,
 	SUBI	= 5'b00111;
 
-reg [data_size-1 : 0] mem_reg[2**data_size-1 : 0];
-initial mem_reg[0] = (( LDI	<< (data_size - 5) )	+ 16	);
-initial mem_reg[1] = (( STO	<< (data_size - 5) )	+ 1	);
-initial mem_reg[2] = (( LD_	<< (data_size - 5) )	+ 1 	);
-initial mem_reg[3] = (( ADDI	<< (data_size - 5) ) + 255	);
-initial mem_reg[4] = (( STO	<< (data_size - 5) ) + 2	);
-initial mem_reg[5] = (( LD_	<< (data_size - 5) ) + 16 );
-initial mem_reg[6] = (( HLT	<< (data_size - 5) ) + 0 );
+reg [data_size-1 : 0] mem_reg[2**addr_bus-1 : 0];
+initial mem_reg[0] = (	( LDI		<< (data_size - 5) )	+ 16	);
+initial mem_reg[1] = (	( STO		<< (data_size - 5) )	+ 1		);
+initial mem_reg[2] = (	( LD_		<< (data_size - 5) )	+ 1 	);
+initial mem_reg[3] = (	( ADDI		<< (data_size - 5) )	+ 255	);
+initial mem_reg[4] = (	( STO		<< (data_size - 5) )	+ 2		);
+initial mem_reg[5] = (	( LD_		<< (data_size - 5) )	+ 16 	);
+initial mem_reg[6] = (	( HLT		<< (data_size - 5) )	+ 0 	);
 
 assign Data = mem_reg[Addr];
 
