01a51df82fd3b0e7ed7ca141a983b7485e18c1a1
fernandeznico <nicofernandez@alumnos.unc.edu.ar>
2017-11-12 18:26:16 -0300

    Se le pone el Reset a la Data_Mem.v No hay funcionalidad de Reset para Prog_Mem.v

--- Data_Mem.v
+++ Data_Mem.v
@@ -23,10 +23,17 @@ end
 
 always @(*)
 begin
+  if( Reset )
+    begin
+    for( pos = 0 ; pos < ( (2**addr_bus) - 1 ) ; pos = pos + 1 )
+      begin
+      mem_reg[pos] = 0;
+      end
+    end
   if( Wr )
-  begin
+    begin
     mem_reg[Addr] = In_Data;
-  end
+    end
 end
 
 assign Out_Data = (Rd)? mem_reg[Addr] : 0;
